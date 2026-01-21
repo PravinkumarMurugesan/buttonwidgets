d6643e89d62e11a89df674f6433e7d271f66e070
PravinkumarMurugesan <smartpravin246@gmail.com>
2026-01-21 10:10:28 +0530

    Initial Commit

---
+++ ButtonWidgets/ButtonWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonWidgetsApp.swift
+//  ButtonWidgets
+//
+//  Created by Pravinkumar on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ButtonWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
